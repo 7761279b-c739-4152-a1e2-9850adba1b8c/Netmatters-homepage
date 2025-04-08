bfaca01b848acde11c52fd00e1c6ba1c235c14db
7761279b-c739-4152-a1e2-9850adba1b8c <203860514+7761279b-c739-4152-a1e2-9850adba1b8c@users.noreply.github.com>
2025-04-08 15:25:04 +0100

    cookies popup save

--- website/js/cookies.js
+++ website/js/cookies.js
@@ -1,12 +1,23 @@
 const consentElement = document.getElementById('manage-consent');
 const cookiesElement = document.getElementById('cookies');
 const acceptElement = document.getElementById('accept-cookies');
+const settingsElement = document.getElementById('cookies-settings');
+
+if (localStorage.getItem('cookies') == undefined) {
+    cookiesElement.style.display = "flex";
+}
 
 consentElement.addEventListener('click', () => {
     cookiesElement.style.display = 'flex';
 })
 
 acceptElement.addEventListener('click', (event) => {
+    cookiesElement.style.display = "none";
+    event.preventDefault();
+    localStorage.cookies = 'all';
+})
+
+settingsElement.addEventListener('click', (event) => {
     cookiesElement.style.display = "none";
     event.preventDefault();
 })
